1409ff1bbb7feab2ac652accfe92f33caf754d66
Mateus de Aquino <mateas1934@gmail.com>
2024-06-29 08:34:59 -0300

    deu certo

--- projeto.py
+++ projeto.py
@@ -1,11 +1,17 @@
 N = int(input("digite o numero de vagas: "))
+while N < 1 or N > 100000:
+    print("valor invalido. Tente de novo")
+    N = int(input("digite o numero de vagas: "))
 M = int(input("digite quantos clientes estão esperando: "))
+while M < 1 or M > 100000:
+    print("valor invalido. Tente de novo")
+    M = int(input("digite quantos clientes estão esperando: "))
 lista_vagas = []
 carros_estacionados = 0
 for i in range(M):
     V_i = int(input("digite sua posição: "))
     while V_i > N or V_i < 1:
-        print("vaga invalida")
+        print("vaga invalida. Tente de novo")
         V_i = int(input("digite sua posição: "))
     if V_i in lista_vagas:
         while V_i in lista_vagas:
